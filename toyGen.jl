a51d7b135721bcf435619f01259938450d74c705
Chi Bach Pham <p.chi.bach@gmail.com>
2022-10-25 14:17:40 +1100

    Update toyGen.jl

--- toyGen.jl
+++ toyGen.jl
@@ -1,12 +1,20 @@
 using SparseArrays
 open("toy.txt", "w") do io
-    n = 316000
-    k = 1
+    n = 1000000
+    k = 5
+    check = zeros(1, n)
     for i = 1:n
-        for j = 1:k
+        repeat = zeros(0)
+        for j = check[i]+1:k
             u = i
-            v = (i + j - 1) % n + 1
+            v = rand(i:n)
+            while (check[v] >= k || v == u || v in repeat)
+                v = rand(i:n)
+            end
             println(io, u, " ", v)
+            repeat = append!(repeat, v)
+            check[v] = check[v] + 1
+            check[u] = check[u] + 1
         end
     end
 end
